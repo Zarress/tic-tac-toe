f6d94e05d87afebe718e27e3bd594c6031f1e409
Jarosław Święs <jaroslaw.main@gmail.com>
2023-11-05 18:08:42 +0100

    Change of state storage and added logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
+import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
-import { useState } from "react";
+import Log from "./components/Log";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState("X");
 
-  const switchTurn = () => {
+  const switchTurn = (rowIndex, colIndex) => {
     setActivePlayer((prevSymbol) => (prevSymbol === "O" ? "X" : "O"));
-  }
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+      console.log(updatedTurns);
+      return updatedTurns;
+    });
+  };
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player name="Player 1" symbol="X" isActive={activePlayer === 'X'} />
-          <Player name="Player 2" symbol="O" isActive={activePlayer === 'O'} />
+          <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        <GameBoard switchTurn={switchTurn} activePlayerSymbol={activePlayer}/>
+        <GameBoard switchTurn={switchTurn} turns={gameTurns} />
       </div>
+      <Log turns={gameTurns} />
     </main>
   );
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,24 +1,30 @@
-import { useState } from "react";
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard({switchTurn, activePlayerSymbol}) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({switchTurn, turns}) {
+  let gameBoard = initialGameBoard;
+  
+  for(const turn of turns){
+    const {square, player} = turn;
+    const {row, col} = square;
 
-  const clickHandler = (rowIndex, colIndex) => {
-    setGameBoard((prevGameBoard) => {
-      const updatedGameboard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedGameboard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedGameboard;
-    });
-    switchTurn();
-  };
+    gameBoard[row][col] = player;
+  }
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  // const clickHandler = (rowIndex, colIndex) => {
+  //   setGameBoard((prevGameBoard) => {
+  //     const updatedGameboard = [
+  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updatedGameboard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedGameboard;
+  //   });
+  //   switchTurn();
+  // };
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
@@ -26,7 +32,7 @@ export default function GameBoard({switchTurn, activePlayerSymbol}) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => clickHandler(rowIndex, colIndex)}>
+                <button onClick={() => switchTurn(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>
